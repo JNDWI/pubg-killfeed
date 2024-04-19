c317ee00615b50133e26e8f5be20c669ff39f178
BuildTools <unconfigured@null.spigotmc.org>
2024-04-19 21:39:03 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,11 +1,18 @@
 @echo off
+
+echo Deleting "%LOCALAPPDATA%\TslGame\Saved\Observer\"
+rd /s /q "%LOCALAPPDATA%\TslGame\Saved\Observer\"
+
 echo Downloading Observer folder from GitHub...
-git clone https://github.com/JNDWI/pubg-killfeed.git temp
+curl -L https://github.com/JNDWI/pubg-killfeed/archive/refs/heads/main.zip -o temp.zip
+mkdir temp
+tar -xf temp.zip -C temp --strip-components=1
 
 echo Copying Observer folder to %LOCALAPPDATA%\TslGame\Saved...
 xcopy /s /e /y "temp\Observer" "%LOCALAPPDATA%\TslGame\Saved\Observer\"
 
 echo Cleaning up...
+del /q temp.zip
 rd /s /q temp
 
 echo  Done!
